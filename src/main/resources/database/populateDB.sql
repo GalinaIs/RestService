2cf9c5f8eb1848e1fe3c73a7c9b1ec3844bf6d98
GalinaIs <denis@isyanov.com>
2021-02-23 12:27:50 +0300

    Добавлена инициализация структуры БД и первоначальное ее заполнение при старте приложения.

---
+++ src/main/resources/database/populateDB.sql
@@ -0,0 +1,7 @@
+INSERT INTO manager (surname, name, patronymic, phone_number) VALUES
+('Surname1', 'Name1', 'Patronymic1', '111-11-11'),
+('Surname2', 'Name2', 'Patronymic2', '222-22-22');
+
+INSERT INTO client (name, address) VALUES
+('Company number 1', 'address1'),
+('Company number 2', 'address2');
