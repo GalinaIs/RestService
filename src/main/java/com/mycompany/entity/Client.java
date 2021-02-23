b85323504ec2beefec3af6d5ce1e2b63afe25c9f
GalinaIs <denis@isyanov.com>
2021-02-23 13:39:55 +0300

    Добавлено связь manager для client.

--- src/main/java/com/mycompany/entity/Client.java
+++ src/main/java/com/mycompany/entity/Client.java
@@ -2,10 +2,7 @@ package com.mycompany.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,19 +12,16 @@ public class Client {
     private Long id;
     private String name;
     private String address;
-    private Long managerId;
+    @ManyToOne
+    @JoinColumn(name = "manager_id", referencedColumnName = "id")
+    private Manager manager;
 
     public Client() {
 
     }
 
     public Client(String name, String address) {
-        this(name, address, null);
-    }
-
-    public Client(String name, String address, Long managerId) {
         this.name = name;
         this.address = address;
-        this.managerId = managerId;
     }
 }
